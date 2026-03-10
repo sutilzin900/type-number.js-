a37562527e924c59611b107cd14a120f4ded8eca
sutilzin900 <jose.henrique.sutil@escola.pr.gov.br>
2026-03-10 14:15:25 +0000

    variaveis

---
+++ var-let-const.js
@@ -0,0 +1,22 @@
+// var
+
+//var altura = 5;
+//var comprimento = 7;
+
+//area = altura * comprimento;
+//console.log(area);
+//var area;
+
+let forma = "reangulo";
+let altura = 5;
+let comprimento = 7;
+let area;
+
+//Se (forma igual  'retangulo')faça
+if (forma === 'retangulo'){
+    area = altura *  comprimento;
+}else{
+    area = (altura = comprimento) / 2;
+}
+console.log(area);
+    
