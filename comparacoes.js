ffd25a16a59c190da09a5a6f285532c9dc1beca8
sutilzin900 <jose.henrique.sutil@escola.pr.gov.br>
2026-03-12 15:02:01 +0000

    comparacao

---
+++ comparacoes.js
@@ -0,0 +1,14 @@
+// == (comparação implicíta)
+
+const numero = 5; 
+const texto = "5";
+
+console.log(numero === texto)
+
+//typerof
+console.log(typeof numero);
+console.log(typeof texto);
+
+//coversão explícíta
+Number()
+String()
