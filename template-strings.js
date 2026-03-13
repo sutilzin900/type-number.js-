cc9f87f3c1d2aeba6893e4bb1d50cadd1a55d0c3
sutilzin900 <jose.henrique.sutil@escola.pr.gov.br>
2026-03-13 15:02:25 +0000

    template final

--- template-strings.js
+++ template-strings.js
@@ -1,3 +1,8 @@
 const nome = "ju";
 const idade = 2026-1981;
 const cidadeDeNascimento = "pitanga";
+
+//const apresentacao = "Meu nome é "+ nome +", minha idade é" + idade +" e nasci na cidade de" + cidadeDeNascimento;
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na  cidade de ${ cidadeDeNascimento}`;
+console.log(apresentacao);
