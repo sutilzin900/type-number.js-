35d9db45d96d15ac75cb2a918b6f3e49d3f122a2
sutilzin900 <jose.henrique.sutil@escola.pr.gov.br>
2026-03-31 13:33:48 +0000

    parametros

--- parametros.js
+++ parametros.js
@@ -5,3 +5,11 @@ fuction soma(numero1, numero2){
  console.log(soma(2, 2));
  console.log(soma(245, 20));
  console.log(soma(-500, 60));
+
+ function nomeIdade(nome, idade){
+    return `meu nome é ${nome} e minha idade é ${idade}`;
+ }
+
+ console.log(nomeIdade("jose", 15));
+ console.log(nomeIdade("Bob", 25));
+ console.log(nomeIdade("Charlie", 35));
