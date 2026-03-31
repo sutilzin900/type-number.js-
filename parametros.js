2dc75b8f89d0164e68d0b1c8c1fcc605acc690ba
sutilzin900 <jose.henrique.sutil@escola.pr.gov.br>
2026-03-31 15:06:21 +0000

    argumentos

--- parametros.js
+++ parametros.js
@@ -1,4 +1,4 @@
-fuction soma(numero1, numero2){
+function soma(numero1, numero2){
     return numero1 + numero2;
  }
 
@@ -11,5 +11,10 @@ fuction soma(numero1, numero2){
  }
 
  console.log(nomeIdade("jose", 15));
- console.log(nomeIdade("Bob", 25));
- console.log(nomeIdade("Charlie", 35));
+
+function multiplica (numero1, numero2){
+      return numero1 * numero2;
+ }
+
+ console.log(multiplica(soma(4, 5),soma(3, 3)))
+ 
